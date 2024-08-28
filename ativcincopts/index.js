f1e4002e6658aa5a6bbdda237d103d7a906d45f0
Leo Eliel <leoeliel13@gmail.com>
2024-08-27 22:09:28 -0400

    prova do faino

---
+++ ativcincopts/index.js
@@ -0,0 +1,20 @@
+const prova1 = document.querySelector("#prova1")
+const prova2 = document.querySelector("#prova2")
+const result = document.querySelector("#result");
+const checkbox = document.querySelector('input[value="mediaa"]');
+
+
+
+form.addEventListener("submit", (e) => 
+    {
+
+        if (checkbox.checked) {
+            let data = (prova1+prova2)/2;
+                   
+        }else{
+
+        }
+
+    }
+);
+
