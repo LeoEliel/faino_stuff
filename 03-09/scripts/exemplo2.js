1c395535050f5482bc191c40d3e124406a579800
Leo Eliel <leoeliel13@gmail.com>
2024-09-03 21:55:46 -0400

    mais mudanças

--- 03-09/scripts/exemplo2.js
+++ 03-09/scripts/exemplo2.js
@@ -2,5 +2,15 @@ let sexo = document.getElementsByName("sexo");
 
 function verificaMarca() {
     let marcado = false;
-    sexo.
+    sexo.forEach(s => {
+        if(s.checked){
+            marcado = true;
+        }
+    });
+    return marcado;
 }
+
+let form = document.getElementsByTagName("form");
+form.onsubmit =  function(){
+    return verificaMarca();
+};
